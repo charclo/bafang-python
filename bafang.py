f1cfd018fca6e9f91558807a86ce119b2bdda33f
Charclo <charclo@pm.me>
2019-11-15 16:20:59 +0100

    Added basic gui mockup (pyqt5)

--- bafang.py
+++ bafang.py
@@ -13,34 +13,101 @@ class Bafang:
         self.checksum = checksum
 
     def store_basic(self, basic_data):
-        self.low_battery_protect = basic_data['low_battery_protect']
-        self.limited_current = basic_data['limited_current']
-        self.limited_current_assist0 = basic_data['limited_current_assist0']
-        self.limited_current_assist1 = basic_data['limited_current_assist1']
-        self.limited_current_assist2 = basic_data['limited_current_assist2']
-        self.limited_current_assist3 = basic_data['limited_current_assist3']
-        self.limited_current_assist4 = basic_data['limited_current_assist4']
-        self.limited_current_assist5 = basic_data['limited_current_assist5']
-        self.limited_current_assist6 = basic_data['limited_current_assist6']
-        self.limited_current_assist7 = basic_data['limited_current_assist7']
-        self.limited_current_assist8 = basic_data['limited_current_assist8']
-        self.limited_current_assist9 = basic_data['limited_current_assist9']
-        self.limited_speed_assist0 = basic_data['limited_speed_assist0']
-        self.limited_speed_assist1 = basic_data['limited_speed_assist1']
-        self.limited_speed_assist2 = basic_data['limited_speed_assist2']
-        self.limited_speed_assist3 = basic_data['limited_speed_assist3']
-        self.limited_speed_assist4 = basic_data['limited_speed_assist4']
-        self.limited_speed_assist5 = basic_data['limited_speed_assist5']
-        self.limited_speed_assist6 = basic_data['limited_speed_assist6']
-        self.limited_speed_assist7 = basic_data['limited_speed_assist7']
-        self.limited_speed_assist8 = basic_data['limited_speed_assist8']
-        self.limited_speed_assist9 = basic_data['limited_speed_assist9']
-        self.wheel_diameter = basic_data['wheel_diameter']
-        self.speedmeter_model = basic_data['speedmeter_model']
-        self.speedmeter_signals = basic_data['speedmeter_signals']
+        self.low_battery_protect = basic_data[0]
+        self.limited_current = basic_data[1]
+        self.limited_current_assist0 = basic_data[2]
+        self.limited_current_assist1 = basic_data[3]
+        self.limited_current_assist2 = basic_data[4]
+        self.limited_current_assist3 = basic_data[5]
+        self.limited_current_assist4 = basic_data[6]
+        self.limited_current_assist5 = basic_data[7]
+        self.limited_current_assist6 = basic_data[8]
+        self.limited_current_assist7 = basic_data[9]
+        self.limited_current_assist8 = basic_data[10]
+        self.limited_current_assist9 = basic_data[11]
+        self.limited_speed_assist0 = basic_data[12]
+        self.limited_speed_assist1 = basic_data[13]
+        self.limited_speed_assist2 = basic_data[14]
+        self.limited_speed_assist3 = basic_data[15]
+        self.limited_speed_assist4 = basic_data[16]
+        self.limited_speed_assist5 = basic_data[17]
+        self.limited_speed_assist6 = basic_data[18]
+        self.limited_speed_assist7 = basic_data[19]
+        self.limited_speed_assist8 = basic_data[20]
+        self.limited_speed_assist9 = basic_data[21]
+        self.wheel_diameter = basic_data[22]
+        self.speedmeter_model = basic_data[23]
+        self.speedmeter_signals = basic_data[24]
+
+    def store_pedal(self, pedal_data):
+        self.low_battery_protect = pedal_data[0]
+        self.limited_current = pedal_data[1]
+        self.limited_current_assist0 = pedal_data[2]
+        self.limited_current_assist1 = pedal_data[3]
+        self.limited_current_assist2 = pedal_data[4]
+        self.limited_current_assist3 = pedal_data[5]
+        self.limited_current_assist4 = pedal_data[6]
+        self.limited_current_assist5 = pedal_data[7]
+        self.limited_current_assist6 = pedal_data[8]
+        self.limited_current_assist7 = pedal_data[9]
+        self.limited_current_assist8 = pedal_data[10]
+        self.limited_current_assist9 = pedal_data[11]
+        self.limited_speed_assist0 = pedal_data[12]
+        self.limited_speed_assist1 = pedal_data[13]
+        self.limited_speed_assist2 = pedal_data[14]
+        self.limited_speed_assist3 = pedal_data[15]
+        self.limited_speed_assist4 = pedal_data[16]
+        self.limited_speed_assist5 = pedal_data[17]
+        self.limited_speed_assist6 = pedal_data[18]
+        self.limited_speed_assist7 = pedal_data[19]
+        self.limited_speed_assist8 = pedal_data[20]
+        self.limited_speed_assist9 = pedal_data[21]
+        self.wheel_diameter = pedal_data[22]
+        self.speedmeter_model = pedal_data[23]
+        self.speedmeter_signals = pedal_data[24]
+
+    def get_basic(self):
+        self.low_battery_protect = basic_data[0]
+        self.limited_current = basic_data[1]
+        self.limited_current_assist0 = basic_data[2]
+        self.limited_current_assist1 = basic_data[3]
+        self.limited_current_assist2 = basic_data[4]
+        self.limited_current_assist3 = basic_data[5]
+        self.limited_current_assist4 = basic_data[6]
+        self.limited_current_assist5 = basic_data[7]
+        self.limited_current_assist6 = basic_data[8]
+        self.limited_current_assist7 = basic_data[9]
+        self.limited_current_assist8 = basic_data[10]
+        self.limited_current_assist9 = basic_data[11]
+        self.limited_speed_assist0 = basic_data[12]
+        self.limited_speed_assist1 = basic_data[13]
+        self.limited_speed_assist2 = basic_data[14]
+        self.limited_speed_assist3 = basic_data[15]
+        self.limited_speed_assist4 = basic_data[16]
+        self.limited_speed_assist5 = basic_data[17]
+        self.limited_speed_assist6 = basic_data[18]
+        self.limited_speed_assist7 = basic_data[19]
+        self.limited_speed_assist8 = basic_data[20]
+        self.limited_speed_assist9 = basic_data[21]
+        self.wheel_diameter = basic_data[22]
+        self.speedmeter_model = basic_data[23]
+        self.speedmeter_signals = basic_data[24]
+
+    def get_info_message(self):
+        return self.manufacturer + self.model + self.hw_version + \
+                self.fw_version + self.voltage + self.max_current + self.checksum
+
+    def get_pedal(self, basic_data):
+        temp = b''
+        for b in basic_data:
+            temp += b
+        return temp
 
 if __name__ == "__main__":
-    baf = Bafang( b'HZXT', b'SZZ6', b'22', b'2011', 1, 20, 27)
-    fetched_data = (b'HZXT', b'SZZ6', b'22', b'2011', 1, 20, 27, b'HZXT', b'SZZ6', b'22', b'2011', 1, 20)
+    baf = Bafang( b'HZXT', b'SZZ6', b'22', b'2011', b'1', b'20', b'27')
+    fetched_data = (b'HZXT', b'0F', b'22', b'2011', b'1', b'20', b'27', b'HZXT', b'SZZ6', b'22',
+                     b'2011', b'1', b'20', b'HZXT', b'SZZ6', b'22', b'2011', b'1', b'20', b'HZXT', b'SZZ6', b'22', b'2011', b'1', b'20')
     baf.store_basic(fetched_data)
     print(baf.limited_current)
+    print(baf.get_pedal(fetched_data))
+    print(baf.get_info_message())
---
+++ pyqt_basic.py
@@ -0,0 +1,40 @@
+from PyQt5 import QtWidgets
+import gui
+import sys
+
+def on_button_clicked():
+    ui.labelManufacturer_2.setText("Bafang")
+    ui.labelModel_2.setText("BBS01")
+    ui.labelHardwVers_2.setText("2.4")
+    ui.labelFirmVers_2.setText("2.0.3")
+    ui.labelMaxCurrent_2.setText("20A")
+
+def actionExitTriggered():
+    app.quit()
+
+def actionLoadTriggered():
+    print("load file")
+
+def actionSaveTriggered():
+    print("save file")
+
+def actionSaveAsTriggered():
+    print("load file")
+
+def prepare_gui():
+    ui.pushButtonLoad.clicked.connect(on_button_clicked)
+    ui.actionExit.triggered.connect(actionExitTriggered)
+    ui.actionLoad.triggered.connect(actionLoadTriggered)
+    ui.actionLoad.triggered.connect(actionLoadTriggered)
+    ui.actionLoad.triggered.connect(actionLoadTriggered)
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = gui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    prepare_gui()
+
+    MainWindow.show()
+    sys.exit(app.exec_())
