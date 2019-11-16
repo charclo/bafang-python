d36b13bc49d1e35ee08f15e0921c7c17415c1cf4
Charclo <charclo@pm.me>
2019-11-16 22:22:26 +0100

    Improved GUI

---
+++ bafang_config.py
@@ -0,0 +1,91 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from generated_gui import Ui_MainWindow
+import sys
+from protocol import Protocol
+from bafang import Bafang
+
+class BafanConfig(Ui_MainWindow, QMainWindow):
+
+    def __init__(self):
+        super(BafanConfig, self).__init__()
+        self.setupUi(self)
+        self.connect_signals()
+        self.connected = False
+    
+    def connect_signals(self):
+        self.pushButtonLoad.clicked.connect(self.on_pushButtonLoad_clicked)
+        self.actionExit.triggered.connect(self.actionExitTriggered)
+        self.actionLoad.triggered.connect(self.actionLoadTriggered)
+        self.actionSave.triggered.connect(self.actionSaveTriggered)
+        self.actionSave_as.triggered.connect(self.actionSaveAsTriggered)
+        self.pushButtonScan.clicked.connect(self.on_pushButtonScan_clicked)
+        self.pushButtonRead.clicked.connect(self.on_pushButtonRead_clicked)
+        self.pushButtonConnect.clicked.connect(self.on_pushButtonConnect_clicked)
+        self.actionAbout.triggered.connect(self.actionAboutTriggered)
+    
+    def on_pushButtonLoad_clicked(self):
+        self.infoBytes = self.protocol.get_info()
+        self.bafang = Bafang(self.infoBytes)
+        self.updateInfo()
+
+    def on_pushButtonScan_clicked(self):
+        self.comboBoxPorts.clear()
+        self.protocol = Protocol()
+        ports_list = self.protocol.get_ports()
+        if len(ports_list) != 0:
+            for p in ports_list:
+                self.comboBoxPorts.addItem(p[0])
+            self.pushButtonConnect.setEnabled(True)
+        else:
+            print("No Com Ports found")
+
+    def on_pushButtonConnect_clicked(self):
+        if not (self.connected):
+            self.protocol.connect(self.comboBoxPorts.currentText())
+            self.connected = True
+            self.pushButtonLoad.setEnabled(True)
+
+        self.statusbar.showMessage("connected to: " + self.comboBoxPorts.currentText())
+        
+
+
+    def on_pushButtonRead_clicked(self):
+        if not (self.connected):
+            self.protocol = Protocol()
+            self.protocol.connect('COM3')
+            self.info = self.protocol.get_info()
+            self.bafang = Bafang(self.info)
+            self.updateInfo()
+            self.connected = True
+            
+
+    def updateInfo(self):
+        self.labelManufacturer_2.setText(self.bafang.manufacturer)
+        self.labelModel_2.setText(self.bafang.model)
+        self.labelHardwVers_2.setText(str(self.bafang.hw_version))
+        self.labelFirmVers_2.setText(str(self.bafang.fw_version))
+        self.labelMaxCurrent_2.setText(str(self.bafang.max_current))
+
+    def actionExitTriggered(self):
+        app.quit()
+
+    def actionLoadTriggered(self):
+        print("load file")
+
+    def actionSaveTriggered(self):
+        print("save file")
+
+    def actionSaveAsTriggered(self):
+        print("save file as")
+
+    def actionAboutTriggered(self):
+        QMessageBox.about(self, "About this app", "This is a cross platform tool to configure bafang controllers.")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = QtWidgets.QMainWindow()
+    bafang_config = BafanConfig()
+    bafang_config.show()
+    app.exec_()
--- protocol.py
+++ protocol.py
@@ -1,14 +1,19 @@
 from serial import Serial
 from time import sleep
+from serial.tools import list_ports
 
 class Protocol():
-    def __init__(self, serial_port):
+    def __init__(self):
         self.connect_cmd = b"\x11\x51\x04\xB0\x05"
+    
+    def connect(self, serial_port):
         self.serial_port = Serial(serial_port, 1200, timeout=1)
         # Write something to arduino to wake it up and then wait 2 seconds until its ready
         self.serial_port.write(self.connect_cmd)
         sleep(2)
-        
+
+    def get_ports(self):
+        return list_ports.comports()
 
 
     def get_info(self):
